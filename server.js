03f31671d7003813211ed358996118986f1f15ff
550007389 <550007389@gehealthcare.com>
2025-07-15 19:50:08 +0530

    update

--- server.js
+++ server.js
@@ -10,7 +10,11 @@ const propertyRoutes = require('./routes/propertyRoutes');
 dotenv.config();
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 app.use(express.json());
 app.use(fileUpload({ useTempFiles: true }));
 
